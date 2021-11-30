a2bf3068c7a081e925ba71953fe8dfa8737bb4a9
Ty Cobb <ty.cobb.m@gmail.com>
2021-11-29 23:03:15 -0500

    add tile animations to gen-rooms

--- bin/lib/gen-rooms.py
+++ bin/lib/gen-rooms.py
@@ -1,4 +1,5 @@
 import click
+import io
 import os
 import png
 import textwrap
@@ -29,7 +30,7 @@ class Config:
     self.only_tiles = only_tiles
 
 # -- model --
-# a tile id (a counter)
+# a id (a counter)
 class Id:
   # -- lifetime --
   def __init__(self, val: int = 0):
@@ -49,6 +50,55 @@ class Id:
   def copy(self):
     return Id(self.val)
 
+# a handle to an image
+class Image:
+  # -- lifetime --
+  def __init__(self, file: io.BufferedReader):
+    # the file handle
+    self.file = file
+
+    # the image data
+    self.data = list(png.Reader(file).read()[2])
+
+  # -- commands --
+  # closes the image and clears its data
+  def close(self):
+    self.file.close()
+    self.data.clear()
+
+  # -- queries --
+  # encode a square slice into an int
+  def slice(self, x: int, y: int, s: int) -> int:
+    # get the squares min / max px coords
+    x0 = x * s
+    x1 = x0 + s
+    y0 = y * s
+    y1 = y0 + s
+
+    # pack the data row-by-row into an int
+    s = 0
+    i = 0
+
+    # for each row
+    for yi in range(y0, y1):
+      # for each pixel
+      for xi in range(x0, x1):
+        # if the pixel is on, set this bit to 1
+        if self.data[yi][xi] == 2:
+          s |= 1 << i
+
+        i += 1
+
+    return s
+
+  # -- factories --
+  # open the image at the path
+  @classmethod
+  def open(cls, path: str):
+    return cls(
+      file = open(path, "rb")
+    )
+
 # a room
 class Room:
   # -- constants --
@@ -62,16 +112,37 @@ class Room:
     # the name of the room (and filename)
     self.name = name
 
+    # the number of frames this room has
+    self.n = 0
+
+  # -- commands --
+  # add to the frame count
+  def add_frame(self):
+    self.n += 1
+
   # -- queries --
   # encode as a tuple of bitsydata: (room, tiles)
   def encode(self, rid: Id, tid: Id) -> T.Tuple[str, str]:
-    # open the png file
-    f = open(osp.join(self.cfg.path, self.name + ".png"), "rb")
-    r = png.Reader(file=f)
+    dir = self.cfg.path
+    nme = self.name
 
-    # get the image data
-    img = r.read()
-    dat = list(img[2])
+    # open images for each frame
+    imgs: list[Image] = []
+    for i in range(self.n):
+      p: T.Optional[str] = None
+
+      # if frame 1, see if an image w/o frame index exists
+      if i == 0:
+        p0 = osp.join(dir, "{0}.png".format(nme))
+        if osp.exists(p0):
+          p = p0
+
+      # otherwise, check for an image w/ frame index
+      if p == None:
+        p = osp.join(dir, "{0}@{1}.png".format(nme, i))
+
+      # add the image
+      imgs.append(Image.open(p))
 
     # build a list of tiles
     tiles: list[Tile] = []
@@ -79,34 +150,23 @@ class Room:
     # for each tile in the room (x, y)
     for y in range(Room.SIZE):
       for x in range(Room.SIZE):
-        # get the tile's pixel coords
-        x0 = x * Tile.SIZE
-        x1 = x0 + Tile.SIZE
-        y0 = y * Tile.SIZE
-        y1 = y0 + Tile.SIZE
+        slices = [ i.slice(x, y, s = Tile.SIZE) for i in imgs ]
 
-        # build the tile grid, a bitmask
-        ti = 0
-        tgrid = 0
+        # if the slices are all empty
+        empty = True
+        for f in slices:
+          if f != 0:
+            empty = False
 
-        # for each row
-        for yi in range(y0, y1):
-          # for each pixel
-          for xi in range(x0, x1):
-            # if the pixel is on, set this bit to 1
-            if dat[yi][xi] == 2:
-              tgrid |= 1 << ti
-            ti += 1
-
-        # if empty, use empty tile
-        if tgrid == 0:
+        # use empty tile
+        if empty:
           tiles.append(Tile.empty())
         # otherwise create a tile
         else:
           tiles.append(Tile(
-            id   = tid.copy(),
-            name = "{0} ({1},{2})".format(self.name, x, y),
-            grid = tgrid
+            id     = tid.copy(),
+            name   = "{0} ({1},{2})".format(self.name, x, y),
+            frames = slices
           ))
 
         # increment the id
@@ -139,7 +199,7 @@ class Room:
       self.name
     )
 
-    # build the tile string
+    # build the tile str
     tstr = ""
     for tile in tiles:
       # from any non-empty tiles
@@ -149,8 +209,9 @@ class Room:
     # incrememnt the room id
     rid.advance()
 
-    # close the file
-    f.close()
+    # close the image files
+    for img in imgs:
+      img.close()
 
     return (rstr, tstr)
 
@@ -160,37 +221,46 @@ class Tile:
   SIZE = 8
 
   # -- lifetime --
-  def __init__(self, id: Id, name: str, grid: int):
+  def __init__(self, id: Id, name: str, frames: list[int]):
     # the id
     self.id = id
 
     # the name
     self.name = name
 
-    # the pixel grid
-    self.grid = grid
+    # the number of frames
+    self.n = len(frames)
+    assert self.n == 1 or self.n == 2, "must have 1 or 2 frames"
+
+    # the list of frames
+    self.frames = frames
 
   # -- queries --
   # if the tile is empty
   def is_empty(self) -> bool:
-    return self.grid == 0
+    return self.id.val == 0
+
+  # if the list of frames is empty
+  @staticmethod
+  def is_frames_empty(frames: list[int]) -> bool:
+    for f in frames:
+      if f != 0:
+        return False
+
+    return True
 
   # encode the tile as bitsydata
   def encode(self):
-    i0 = 0
-    i1 = Tile.SIZE * Tile.SIZE
+    # format the frames
+    nf = self.n
+    fs = self.frames
 
-    # format the grid string
-    tgrid = ""
-    for i in range(i0, i1):
-      if (self.grid & (1 << i)) != 0:
-        tgrid += "1"
-      else:
-        tgrid += "0"
+    # encode the first frame
+    fstr = self.encode_frame(fs[0])
 
-      ii = i + 1
-      if (i + 1) % 8 == 0 and ii != i1:
-        tgrid += "\n"
+    # if the second frame is different, add it
+    if nf == 2 and fs[0] != fs[1]:
+      fstr += "\n>\n{0}".format(self.encode_frame(fs[1]))
 
     # format the tile
     tfmt = """
@@ -202,18 +272,40 @@ class Tile:
 
     return hdoc(tfmt).format(
       self.id.encode(),
-      tgrid,
+      fstr,
       self.name
     )
+
+  # encode the frame as a string
+  def encode_frame(self, f: int) -> str:
+    i0 = 0
+    i1 = Tile.SIZE * Tile.SIZE
+
+    # for each bit
+    str = ""
+    for i in range(i0, i1):
+      # if it's on, "1"
+      if (f & (1 << i)) != 0:
+        str += "1"
+      # otherwise it's off, "0"
+      else:
+        str += "0"
+
+      # and a newline at row end
+      ii = i + 1
+      if (i + 1) % Tile.SIZE == 0 and ii != i1:
+        str += "\n"
+
+    return str
 
   # -- factories --
   # create an empty tile
   @classmethod
   def empty(cls):
     return cls(
-      id   = Id(),
-      name = "empty",
-      grid = 0b0,
+      id     = Id(),
+      name   = "empty",
+      frames = [0b0],
     )
 
 # -- command --
@@ -225,17 +317,25 @@ class GenRooms:
 
   def __call__(self):
     # find all rooms
-    rooms = []
+    rooms: dict[str, Room] = {}
 
     # for each path in the dir
     d = self.cfg.path
     for f in os.listdir(d):
-      p = osp.join(d, f)
-
       # if it's a png, add a room
       name, ext = osp.splitext(f)
-      if osp.isfile(p) and ext == ".png":
-        rooms.append(Room(self.cfg, name=name))
+
+      if osp.isfile(osp.join(d, f)) and ext == ".png":
+        # get the name and frame, if any
+        name, _ = name.split("@")
+
+        # find or create the room
+        room = rooms.get(name)
+        if room == None:
+          room = rooms[name] = Room(self.cfg, name)
+
+        # incr the frame count
+        room.add_frame()
 
     # track a shared room id and tile id (tile 0 is empty)
     rid = Id()
@@ -249,7 +349,7 @@ class GenRooms:
     tstr += "{0}\n".format(Tile.empty().encode())
 
     # for each room
-    for i, r in enumerate(rooms):
+    for i, r in enumerate(rooms.values()):
       r, t = r.encode(rid, tid)
       rstr += r
       tstr += t
